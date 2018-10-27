7bcaa45b672a6d3a84858d9b4bf5778145a4d89e
Kirby Campbell <jkirbycampbell@gmail.com>
2018-10-27 00:57:39 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,10 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
-    
   end
 
+  def body
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
 end
