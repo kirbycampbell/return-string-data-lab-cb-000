f23114e7b41d359f04e36b73552d2757d6bbbd82
Kirby Campbell <jkirbycampbell@gmail.com>
2018-10-27 01:01:50 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,12 @@ class ProductsController < ApplicationController
   @product = Product.new
 end
 
+  def create
+    @product = Product.create(product_params)
+    @product.save
+    redirect_to product_path(@product)
+  end
+
   def body
     product = Product.find(params[:id])
     render plain: product.description
